0e2d78162aa3d009ee5faebc2238870962316539
DEVENDRA70672976 <163308392+DEVENDRA70672976@users.noreply.github.com>
2025-07-25 16:22:08 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is java programming ");
+        System.out.println("Output frome java program");
+        System.err.println(new Date());
+    }
+}
